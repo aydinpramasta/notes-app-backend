0691f35bb01eff2c43914f3086edc305e08620f8
Aydin Pramasta <aydin.pram@gmail.com>
2023-03-23 09:54:56 +0700

    init

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+const init = async () => {
+    const server = Hapi.server({
+        host: "localhost",
+        port: 8080,
+        routes: {
+            cors: {
+                origin: ["http://notesapp-v1.dicodingacademy.com"],
+            },
+        },
+    });
+
+    server.route(routes);
+
+    await server.start();
+
+    console.log(`Server berjalan di ${server.info.uri}`);
+};
+
+init();
